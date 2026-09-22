3cef2f0736bee08207a8c351a53a5a0497801ebe
Asakhe-S <asakhesotyato1@gmail.com>
2026-09-22 11:17:44 +0200

    Added number 3 to assignment2

--- assigment2.py
+++ assigment2.py
@@ -30,8 +30,16 @@
         #else:
             #print("no discount")
             
-        #print(f"Grand total: ${grand_total} \n Total saved: ${total_saved} \n Final total: ${final_total}")  
+        #print("=============================================")
+        #print (f"Grand total: ${grand_total}")
+        #print("=============================================")
+        #print(f"Total saved: ${total_saved}")
+        #print("=============================================")
+        #print(f"Final total: ${final_total}")
         #break
+        
+        
+        
     #else:
         #print("item not found in Catalog. try again")
         #break
@@ -39,40 +47,74 @@
     
 2.
 
-count = int(input("How many student enteries do you want to create?")) 
-student_records = {}
+#count = int(input("How many student enteries do you want to create?")) 
+#student_records = {}
 
-for i in range(count):
-    student_name = input("Enter student name: ")
-    student_score = int(input("Enter student score: "))
-    student_records[student_name] = student_score
+#for i in range(count):
+    #student_name = input("Enter student name: ")
+    #student_score = int(input("Enter student score: "))
+    #student_records[student_name] = student_score
     
-    passed_students = 0
+    #passed_students = 0
     
-    for student, score in student_records.items():
-        grade = ["A", "B", "C"]
-        status = ["Passed with distinction", "Passed", "needs improvement"]
+    #for student, score in student_records.items():
+        #grade = ["A", "B", "C"]
+        #status = ["Passed with distinction", "Passed", "needs improvement"]
         
-        if score >= 70:
-            passed_students += 1
-            print(f"{student} scored {score} and got grade {grade[0]} and status {status[0]}")
+        #if score >= 70:
+            #passed_students += 1
+            #print(f"{student} scored {score} and got grade {grade[0]} and status {status[0]}")
             
-        elif score >= 50:
-            passed_students += 1
-            print(f"{student} scored {score} and got grade {grade[1]} and status {status[1]}")
+        #elif score >= 50:
+            #passed_students += 1
+            #print(f"{student} scored {score} and got grade {grade[1]} and status {status[1]}")
             
-        else :
-            print(f"{student} scored {score} and got grade {grade[2]} and status {status[2]}")
+        #else :
+            #print(f"{student} scored {score} and got grade {grade[2]} and status {status[2]}")
             
-total_score = sum(student_records.values())
-class_average = total_score / count
+#total_score = sum(student_records.values())
+#class_average = total_score / count
 
-failed_students = count - passed_students
+#failed_students = count - passed_students
 
-print(f"Total number of students who passed: {passed_students}")
-print(f"Total number of students who failed: {failed_students}")
-print(f"Class average: {class_average}")
+#print(f"Total number of students who passed: {passed_students}")
+#print(f"Total number of students who failed: {failed_students}")
+#print(f"Class average: {class_average}")
     
         
+3
+
+Raw_user_records = ("userID", "Name", "Role", "Is_Active", "Login_Attempt")   
+users =[(101, "Alice", "admin", True, 1),
+        (102, "Bob", "member", True, 4),
+        (103, "Charlie", "editor", False, 0),
+        (104, "Diana", "admin", False, 6),
+        (105, "Even", "member", True, 2),
+        (106, "Fiona", "guest", True, 0)
+        ]
+total_active = 0
+
+for user in users:
+    if user[4] >= 5:
+        print(f"Account for {user[1]} is locked due to too many login attempts.")
     
+    if user[3] == True and user[2] == "admin":
+        total_active += 1
+        print(f"Full system access granted to {user[1]} with userID {user[0]}.")
+        
+    elif user[3] == True and user[2] == "editor" or user[2] == "member":
+        total_active += 1
+        print(f"Standard system access granted to {user[1]} with userID {user[0]}.")
+        
+    elif user[3] == True and user[2] == "guest":
+            total_active += 1
+            print(f"Standard system access granted to {user[1]} with userID {user[0]}.")
+        
+    else:
+        print(f"Account for {user[1]} is inactive)")
+        
+total_inactive = len(users) - total_active
+total_security_alerts = sum(1 for user in users if user[4] >= 5)
+
+print(f"\n Total Active Users: {total_active} \n Total Inactive Users: {total_inactive} \n Total Security Alerts: {total_security_alerts}")
     
